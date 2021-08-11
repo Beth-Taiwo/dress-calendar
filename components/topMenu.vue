a01910370d9e5785c429e76a8e2f5826635c8a72
Beth-Taiwo <popoolataiwo22@gmail.com>
2021-08-11 16:55:02 +0000

    WIP:top menu

---
+++ components/topMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <header
+    class="bg-caLightGray rounded-3xl rounded-b-none p-4 text-center relative"
+  >
+    <div
+      class="absolute inset-y-1/4 bg-white w-10 h-8 flex justify-center items-center rounded-md"
+      style="left: -10px"
+    >
+      <i class="fas fa-bars"></i>
+    </div>
+    <p class="text-transBlack font-medium">Monday 12th - Thursday 16th</p>
+    <div
+      class="absolute inset-y-1/4 bg-white w-10 h-8 flex justify-center items-center rounded-md"
+      style="right: -10px"
+    >
+      <i class="fas fa-calendar"></i>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
