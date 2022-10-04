c065ff32e3d99207aaf411e14fa007f7009ebbdb
Beth Taiwo <popoolataiwo22@gmail.com>
2022-10-04 22:27:39 +0000

    badge component setup

---
+++ components/Badge.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="bg-[#4A4A4A] text-[#E5E5E5] text-sm text-center shadow-md rounded-3xl px-2 py-1"
+  >
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Badge',
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style></style>
